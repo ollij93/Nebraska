17d154e12cbab92c4b14f6aeaf1d99dc158fa621
Olli Johnson <ollij93@outlook.com>
2018-04-05 15:42:53 +0100

    Fixed webui to use nebraska Session and be bootable on unix

--- webui/main/views.py
+++ webui/main/views.py
@@ -7,28 +7,31 @@ import os
 import sys
 sys.path.insert(0, '../../nebraska')
 
-from nebraska import common
+from nebraska.session import Session
 
 # Create your views here.
 def index(requests):
-	cache = json.load(open(common.NEBRASKA_DIR + '/cache.json'))
-	transactions = sorted(cache["accounts"][0]["transactions"], key=lambda t: t["date"])
+    session = Session()
+    session.load()
 
-	js_dates = '['
-	js_balnc = '['
+    transactions = sorted(session.accounts[0].get_transactions(),
+                          key=lambda t: t.date)
 
-	num_transactions = len(transactions)
+    js_dates = '['
+    js_balnc = '['
 
-	for i, tran in enumerate(transactions):
-		js_dates += '"' + tran["date"] + '", '
+    num_transactions = len(transactions)
 
-		js_balnc += str(tran["balance_after"]) + ', '
+    for i, tran in enumerate(transactions):
+        js_dates += '"' + tran.date + '", '
 
-	js_dates = js_dates[:-2] + ']'
-	js_balnc = js_balnc[:-2] + ']'
+        js_balnc += str(tran.balance_after) + ', '
 
-	parameters = {}
-	parameters['js_dates'] = js_dates
-	parameters['js_balnc'] = js_balnc
+    js_dates = js_dates[:-2] + ']'
+    js_balnc = js_balnc[:-2] + ']'
 
-	return render(requests, 'main/index.html', parameters)
+    parameters = {}
+    parameters['js_dates'] = js_dates
+    parameters['js_balnc'] = js_balnc
+
+    return render(requests, 'main/index.html', parameters)
