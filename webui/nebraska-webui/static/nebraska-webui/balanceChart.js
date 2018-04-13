182f6b24f50ba44bc9319a85435811a67e67745c
GEOFF\James <jimyj@hotmail.com>
2018-04-13 22:12:30 +0100

    fixed (or hacked) module importing and updates to UI

--- webui/nebraska-webui/static/nebraska-webui/balanceChart.js
+++ webui/nebraska-webui/static/nebraska-webui/balanceChart.js
@@ -1,12 +1,17 @@
 window.onload = function() {
-    const ctx = document.getElementById('balanceChart').getContext("2d");
+    const ctx = document.getElementById('balance-chart').getContext("2d");
     const chart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ["0"],
-            datasets: [{"data": [0]}]
+            datasets: [{
+                "data": [0]
+            }]
         },
         options: {
+            legend: {
+                display: false
+            },
             layout: {
                 padding: {
                     left: 20,
@@ -14,6 +19,23 @@ window.onload = function() {
                     top: 20,
                     bottom: 20
                 }
+            },
+            responsive: false,
+            elements: {
+                point: {
+                    radius: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        autoSkip: true,
+                        maxTicksLimit: 20,
+                        callback: function(value) {
+                            return new Date(value).toLocaleDateString('en', {month:'short', year:'numeric'});
+                        }
+                    }
+                }]
             }
         }
     });
